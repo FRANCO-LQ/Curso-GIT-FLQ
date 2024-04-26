59b84d1eb17174cbbae358cdd3ba920412871487
Franco <jackfrac78@gmail.com>
2024-04-25 20:22:22 -0400

    ADICION Y REPOSITORIO, CARATULA Y CREACION DE UN EJEMPLO

--- curso git/ej 1.py
+++ curso git/ej 1.py
@@ -1,5 +1,3 @@
-
-print(type(decimal))
 print('''Hola 
 desde 
 LASIN''')
@@ -7,5 +5,4 @@ print('Gabriel\t\t50')
 print('Evelin\t\t100')
 print('Pedro\t\t50')
 print('Sandra\t\t100')
-
 print(r"c:")
